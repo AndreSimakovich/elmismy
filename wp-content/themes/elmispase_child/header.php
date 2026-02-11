96f2247d6d78f69535394c880f00d09e08230bd2
andreSimakovich <simagom16@gmail.com>
2026-02-11 10:08:24 +0300

    Определена концепция

--- wp-content/themes/elmispase_child/header.php
+++ wp-content/themes/elmispase_child/header.php
@@ -55,11 +55,9 @@ if ( ! function_exists( 'of_get_option' ) ) {
 
     <!-- Контейнер с контентом -->
     <div class="header-overlay-content">
-        <h1 class="hero-title">«ЭЛМИС» — вдыхаем новую жизнь в ваше оборудование</h1>
-        <p class="hero-subtitle">На высоте новых технологий</p>
-            <!-- Кнопки теперь в общем флекс-контейнере -->
-    <div class="hero-buttons-wrap">
-        <a href="#callback-form" class="btn-callback popmake-975">Рассчитать проект</a>
+        <h1 class="hero-title">На высоте новых технологий</h1>
+        <p class="hero-subtitle">«Элмис» — вдыхаем новую жизнь в ваше оборудование</p>
+         <a href="#callback-form" class="btn-callback popmake-975">Рассчитать проект</a>
         <a href="<?php echo esc_url( get_permalink( 931 ) ); ?>" class="hero-link">
             Получить консультацию 
         </a>
